28cb184f62fdde8e91627e7e0553e407afdf1dd8
dipanjal <dipanjalmaitra@gmail.com>
2019-05-20 13:13:08 +0600

    docker connection added

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-module.exports.SequelizeDBConfig = {
+/*module.exports.SequelizeDBConfig = {
     database: 'ra_houzes',
     username: 'postgres',
     password: 'abc123',
@@ -7,33 +7,34 @@ module.exports.SequelizeDBConfig = {
     timezone: '+06:00',
     host: 'localhost',
     port: 5432
-};
+};*/
 
-// module.exports.SequelizeDBConfig = {
-//     database: 'ra_houzes',
-//     username: 'wsit',
-//     password: 'wsit',
-//     dialect: 'postgres', // PostgreSQL, MySQL, MariaDB, SQLite and MSSQL See more: http://docs.sequelizejs.com/en/latest/
-//     // logging: console.log,
-//     timezone: '+06:00',
-//     host: 'localhost',
-//     port: 5432
-// };
+/*module.exports.SequelizeDBConfig = {
+    database: 'ra_houzes',
+    username: 'wsit',
+    password: 'wsit',
+    dialect: 'postgres', // PostgreSQL, MySQL, MariaDB, SQLite and MSSQL See more: http://docs.sequelizejs.com/en/latest/
+    // logging: console.log,
+    timezone: '+06:00',
+    host: 'localhost',
+    port: 5432
+};*/
 
 /**
  * Docker postgres db conf
  * @type {{database: string, password: string, dialect: string, port: number, timezone: string, host: string, username: string}}
  */
-// module.exports.SequelizeDBConfig = {
-//     database: 'ra_houzes',
-//     username: 'root',
-//     password: 'wsit97480',
-//     dialect: 'postgres', // PostgreSQL, MySQL, MariaDB, SQLite and MSSQL See more: http://docs.sequelizejs.com/en/latest/
-//     timezone: '+06:00',
-//     // host: 'localhost',
-//     host: '172.18.1.11',
-//     port: 5454
-// };
+
+module.exports.SequelizeDBConfig = {
+    database: 'ra_houzes',
+    username: 'root',
+    password: 'wsit97480',
+    dialect: 'postgres', // PostgreSQL, MySQL, MariaDB, SQLite and MSSQL See more: http://docs.sequelizejs.com/en/latest/
+    timezone: '+06:00',
+    // host: 'localhost',
+    host: '172.18.1.11',
+    port: 5454
+};
 
 module.exports.otpLifeTimeInMIlli = (60*60*24*1000)*2;
 module.exports.otpLifeTimeInDays = 2;
