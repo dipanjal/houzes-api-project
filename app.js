7ec6e7dfff1a2a487747d10cca20ef80dbbac009
dipanjal <dipanjalmaitra@gmail.com>
2019-06-14 12:08:57 +0600

    live tracking module structure changed

--- app.js
+++ app.js
@@ -9,9 +9,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-app.get('/',function(req, res){
-	res.sendFile(__dirname+'/modules/live_tracking/client.html');
-});
 
 app.use('/oauth',require('./modules/oauth/oauth-controller'));
 app.use('/api',require('./routers/api-router'));
@@ -27,35 +24,16 @@ server.listen(port, () => {
 	console.log(`listening: ${port}`);
 });
 
-/**
- * calling socket
- */
-let liveTracking = require('./modules/live_tracking')(io);
-
 
 /**
- * SOCKET STARTS HERE
+ * calling socket chatroom
  */
+require('./modules/live_tracking/chatting')(io);
 
-// context = {};
-// context.username = 'user1';
-// io.on('connection', function(socket){
-// 	socket.on('chat message', function(msg){
-// 		io.emit('chat message', msg);
-// 	});
-//
-// 	socket.on('authenticate user', (data) => {
-// 		if (context.username === data.username){
-// 			socket.username = data.username;
-// 			io.emit('authenticated', 'user authentication successful!!!');
-// 		}else{
-// 			io.emit('unauthenticated', 'failed to authentication!!!');
-// 		}
-// 	});
-//
-// 	socket.on('disconnect', () => {
-// 		console.log(`${socket.username} disconnected`);
-// 	});
-// });
-
-
+/**
+ * serving html file
+ * A Client for realtime chatting
+ */
+app.get('/',function(req, res){
+	res.sendFile(__dirname+'/modules/live_tracking/chat-client.html');
+});
--- modules/live_tracking/location-listener.js
+++ modules/live_tracking/location-listener.js
@@ -1,14 +1,4 @@
-/**
- * passing server instance from app.js to live_tracking module
- * @param server
- */
-
 module.exports = (io) => {
-
-    let context = {};
-    context.username = 'user1';
-
-    let rooms={};
 
     io.on('connection', function(socket){
 
@@ -50,38 +40,5 @@ module.exports = (io) => {
         //     }
         //
         // });
-
-
-
-        socket.on('join request', data => {
-            // let roomName = data.roomName;
-
-            // if(rooms[roomName]){
-            //     rooms[roomName].push(socket);
-            // }else{
-            //     rooms[roomName] = [socket];
-            // }
-
-            socket.join(data.roomName);
-            io.to(socket.id).emit('me joined', data);
-            socket.broadcast.to(data.roomName).emit('new user',data);
-
-            // io.to(roomName).emit('new user',{
-            //     roomName:roomName,
-            //     username:data.username
-            // });
-
-        });
-
-        socket.on('room chatting', data=> {
-            io.to(data.roomName).emit('room message',data);
-        });
-
-        socket.on('leave request', data => {
-            // socket.leave(data.roomName);
-            io.to(socket.id).emit('me left',data);
-            socket.broadcast.to(data.roomName).emit('left user',data);
-        });
-
     });
 };
